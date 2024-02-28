11daff6ede0f50be1bc7b5859b4056522959a657
SAHI <129284681+rayangosling90@users.noreply.github.com>
2024-02-28 10:52:53 +0530

    add : sign option
    created login and register page for users
    create files : sign html / sign css / sign js

---
+++ project_2024/sign.js
@@ -0,0 +1,9 @@
+function showloginform(){
+    document.getElementById('div1').style.display = 'none';
+    document.getElementById('div2').style.display = 'block';
+}
+function showregisterform(){
+    document.getElementById('div2').style.display = 'none';
+    document.getElementById('div1').style.display = 'block';
+}
+
